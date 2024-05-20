278f67bad1c81c7fd6e2ca7aae5889d1cbe6c148
mahesh <m2wankhede@gmail.com>
2024-05-20 12:36:54 +0530

    added project directory and simple setup

---
+++ src/test/java/com/qa/todo/baseSetup/baseSetup.java
@@ -0,0 +1,10 @@
+package com.qa.todo.baseSetup;
+
+public class baseSetup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/qa/todo/tests/createToDo.java
@@ -0,0 +1,5 @@
+package com.qa.todo.tests;
+
+public class createToDo {
+
+}
