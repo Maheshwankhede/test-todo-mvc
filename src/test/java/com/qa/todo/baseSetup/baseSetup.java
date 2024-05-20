50505600c342e3ddc51af039711df5156a497abe
mahesh <m2wankhede@gmail.com>
2024-05-20 14:18:21 +0530

    Fixed issues, updated testng version

--- src/test/java/com/qa/todo/baseSetup/baseSetup.java
+++ src/test/java/com/qa/todo/baseSetup/baseSetup.java
@@ -1,5 +1,8 @@
 package com.qa.todo.baseSetup;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
@@ -14,15 +17,36 @@ public class baseSetup {
 	protected WebDriver driver;
 	private static Properties properties;
 	
-	public static String getAppUrl() {
-        return properties.getProperty("app_url");
-    }
+	public static Properties init_properties() {
+		properties = new Properties();
+
+		try {			
+			FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "/src/test/java/com/qa/todo/utils/config.properties");
+			properties.load(ip);
+		} catch (FileNotFoundException e) {
+			System.out.println("config file is missing, please check it...");
+		} catch (IOException e) {
+			System.out.println("properties could not be loaded...");
+			e.printStackTrace();
+		}
+		return properties;
+	}
+
+	 public static String getAppUrl() {
+	        // Initialize properties if not already initialized
+	        if (properties == null) {
+	            properties = init_properties();
+	        }
+	        return properties.getProperty("app_url");
+	    }
 	
 	@BeforeMethod
 	public void launchBrowser() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        System.out.println(getAppUrl());
+        driver.get(getAppUrl());
 	}
 
 	@AfterMethod
