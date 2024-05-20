53dfded4a2fa66f93ae22e96a67072ed0074e91c
mahesh <m2wankhede@gmail.com>
2024-05-20 12:56:19 +0530

    created base test setup and configs

--- src/test/java/com/qa/todo/baseSetup/baseSetup.java
+++ src/test/java/com/qa/todo/baseSetup/baseSetup.java
@@ -1,10 +1,34 @@
 package com.qa.todo.baseSetup;
 
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class baseSetup {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	protected WebDriver driver;
+	private static Properties properties;
+	
+	public static String getAppUrl() {
+        return properties.getProperty("app_url");
+    }
+	
+	@BeforeMethod
+	public void launchBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
 	}
 
+	@AfterMethod
+	public void closeBrowser() {
+		if (driver != null) {
+            driver.quit();
+        }
+	}
 }
